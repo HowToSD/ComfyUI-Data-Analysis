e3d5e65d79299c1aa27a0fe9b1ae99338cb2b420
HowToSD <support@candee.ai>
2025-02-20 00:22:29 -0800

    Updated docstring for Pandas Rename node to clarify the node parameter.

--- modules/pandas_wrapper/pandas_rename.py
+++ modules/pandas_wrapper/pandas_rename.py
@@ -6,9 +6,8 @@ class PandasRename:
     """
     Renames an index (row label) or a column label.
 
-    Note below node parameter description is inaccurate due to a reference generator's bug:
-    * axis takes columns or index, not DataFrame.
-    * Both current_label and new_label take a String.
+    Note below node parameter description is incomplete due to a reference generator's bug:
+    * axis takes columns or index.
 
     category: Transformation
     """
